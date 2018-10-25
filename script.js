6f68178b389f9d83f9a78f3e46d8a5b11dfc9a10
mfadlipadillah <43973574+mfadlipadillah@users.noreply.github.com>
2018-10-25 15:58:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+$("#myArea").fadeIn(1000);
+$("#flip1").click(function(){
+	$("#myTable").slideToggle();
+});
+$("#flip2").click(function(){
+	$("#myForm").slideToggle();
+});
+$("#btnForm").click(function( event ) {
+  if ( $( "#Input1 " ).val() === "" ) {
+    alert("NPM tidak boleh kosong!");
+    return;
+  }
+  if ( $( "#Input2" ).val() === "" ) {
+    alert("Nama tidak boleh kosong!");
+    return;
+  }
+  if ( $( "#Input3" ).val() === "" ) {
+    alert("Tgl Lahir tidak boleh kosong!");
+    return;
+  }
+  alert("Data valid");
+});
