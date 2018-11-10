3709e8ae1a87739156e4ee06c59a36452f48c8e6
mfadlipadillah <43973574+mfadlipadillah@users.noreply.github.com>
2018-11-11 00:00:18 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,20 @@ $("#flip1").click(function(){
 $("#flip2").click(function(){
 	$("#myForm").slideToggle();
 });
-$("#btnForm").click(function( event ) {
+$("#Form1").submit(function( event ) {
   if ( $( "#Input1 " ).val() === "" ) {
     alert("NPM tidak boleh kosong!");
+	event.preventDefault();
     return;
   }
   if ( $( "#Input2" ).val() === "" ) {
     alert("Nama tidak boleh kosong!");
+	event.preventDefault();
     return;
   }
   if ( $( "#Input3" ).val() === "" ) {
     alert("Tgl Lahir tidak boleh kosong!");
+	event.preventDefault();
     return;
   }
   alert("Data valid");
